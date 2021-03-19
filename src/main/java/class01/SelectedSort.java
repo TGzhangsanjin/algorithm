c623ca1ba56191bca3328040485d549a2ba4c96d
zhangxin <>
2021-03-19 22:41:14 +0800

    选择排序的提交

--- src/main/java/class01/SelectedSort.java
+++ src/main/java/class01/SelectedSort.java
@@ -1,5 +1,7 @@
 package class01;
 
+import utils.Util01;
+
 /**
  *
  * 选择排序
@@ -11,6 +13,30 @@ package class01;
 public class SelectedSort {
 
     public static void sorted(int[] array) {
-        //
+        if (array == null || array.length == 1) {
+            return;
+        }
+        int minIndex;
+        for (int i = 0; i < array.length; i++) {
+            minIndex = i;
+            // 每次循环将右侧的最小值与左侧排好序的末尾+1下标的值做交换
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            Util01.swapArray(array, i , minIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTimes = 100;
+        int oneTimeNums = 100;
+        int range = 100;
+        int[] array = Util01.randomArray(oneTimeNums, range);
+        sorted(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
     }
 }
