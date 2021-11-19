7ab6a5cc9154e73116a7ad814af0424b96fe0c74
zhangxin <jizhibao>
2021-11-19 14:29:08 +0800

    异或的基本运算

---
+++ src/main/java/class03/TwoOddOtherEven.java
@@ -0,0 +1,16 @@
+package class03;
+
+/**
+ * 问题： 一个数组中，有两种数出现了奇数次，其他数都出现了偶数次，怎么找到并打印这两种数
+ * 思路：
+ * @Author 张三金
+ * @Date 2021/11/19 0019 13:46
+ * @Company jzb
+ * @Version 1.0.0
+ */
+public class TwoOddOtherEven {
+
+    public static void findOdd (int[] array) {
+
+    }
+}
