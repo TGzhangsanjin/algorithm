36f27bc2c89b74bb1f17addfc55850a5afc67c6b
zhangxin <jizhibao>
2021-12-10 11:27:12 +0800

    基数排序 - 不使用额外容器的方式

--- src/main/java/class09/Code03_RadixSort.java
+++ src/main/java/class09/Code03_RadixSort.java
@@ -5,7 +5,7 @@ import utils.ArrayUtil;
 import java.util.PriorityQueue;
 
 /**
- * 基数排序
+ * 基数排序  时间复杂度 O(N), 额外空间复杂度 O(N)
  * 经典的数据范围：非负的，可以用10进制来进行理解的数
  * ps: 其它的数据的话都要尽量转换成经典的数据范围，比如处理负数时，先将所有的数字都加上最小值的相反数，后面再减掉（但是可能会有溢出问题）
  * 思路：
@@ -16,12 +16,79 @@ import java.util.PriorityQueue;
  *      (5) 第三部所有数字根据十位数字进入对应的桶中，然后将所有数字导出来，先进先出
  *      .........................................
  *      .........................................
+ * 不使用容器的方式：
+ *      (1) 先遍历所有的数找出最大值，判断最大的数是几位数 n
+ *      (2) 遍历 n
+ *      (3) 每次遍历时，准备一个长度为10的辅助数组 count, 数组 count 每个下标存的是数组中下标对应的数出现测次数
+ *      (4) 求 count 数组的前缀和数组 countPrefixCount
+ *      (5) 从右往左遍历原数组，判断遍历的数的位数index ,countPrefixCount[index] 的值就是该数在原数组中的位置，然后 countPrefixCount[index]--
  * @Author 张三金
  * @Date 2021/12/10 0010 9:29
  * @Company jzb
  * @Version 1.0.0
  */
 public class Code03_RadixSort {
+
+    // 不使用容器的方式
+    public static void radixSort (int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        radixSort(array, 0, array.length - 1, maxBit(array));
+    }
+
+    public static void radixSort (int[] array, int left, int right, int digit) {
+        int[] help = new int[right - left + 1];
+        // 数组中的最大值时多少位，原数组就进出多少次
+        for (int d = 1; d <= digit; d++) {
+            int[] count = new int[10];
+            // count[0] 当前位(d位)是0的有多少个
+            // count[1] 当前位(d位)是1的有多少个
+            // count[2] 当前位(d位)是1的有多少个
+            for (int i = left; i <= right; i++) {
+                count[getDigit(array[i], d)]++;
+            }
+            // 求 count 变成前缀和
+            for (int i = 1; i < count.length; i++) {
+                count[i] = count[i] + count[i - 1];
+            }
+            // 从右往左遍历数组
+            for (int i = right; i >= left ; i--) {
+                // 当前遍历的数在 d 位上的数字
+                int currentDigit = getDigit(array[i], d);
+                help[count[currentDigit] - 1] = array[i];
+                // 这行注释掉的代码时错误的，一开始想着不使用help数组，直接交换，发现不行，因为交换后当前循环的遍历就会错乱
+//                ArrayUtil.swapTwoNum(array, i, left + count[currentDigit] - 1);
+                count[currentDigit]--;
+            }
+            // 将 help 数组拷贝回去到原数组中
+            for (int i = left; i <= right; i++) {
+                array[i] = help[i - left];
+            }
+        }
+    }
+
+    /**
+     * 取出number 这个数在 d 位上的数字，
+     * 比如：d = 1, 表示取出number 个位数字上的数来
+     *      d = 2, 则去除number 十位数字上的数来
+     */
+    public static int getDigit (int number, int d) {
+        return (int) (number / Math.pow(10, d - 1)) % 10;
+    }
+
+    public static int maxBit(int[] array) {
+        int max = 0;
+        for (int i: array) {
+            max = Math.max(max, i);
+        }
+        int digit = 0;
+        while (max > 0) {
+            digit++;
+            max /= 10;
+        }
+        return digit;
+    }
 
     // 使用容器的方式 1.0
     public static void radixSort01 (int[] array) {
@@ -68,7 +135,8 @@ public class Code03_RadixSort {
         for (int i = 0; i < testTimes; i++) {
             int[] array01 = ArrayUtil.generateRandomArray03(oneTimeNums, range);
             int[] array02 = ArrayUtil.copyArray(array01);
-            radixSort01(array01);
+//            radixSort01(array01);
+            radixSort(array01);
             quickSort(array02, 0, array02.length - 1);
             for (int j = 0; j < array01.length; j++) {
                 if (array01[j] != array02[j]) {
