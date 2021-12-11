f4a836965fbdf74c89877384e69f42a1eff3cdcc
zhangxin <jizhibao>
2021-12-11 16:45:19 +0800

    提交二叉树非递归遍历的测试

--- src/main/java/class11/Code03_NonRecursiveTraversal.java
+++ src/main/java/class11/Code03_NonRecursiveTraversal.java
@@ -16,16 +16,16 @@ public class Code03_NonRecursiveTraversal {
     /**
      * 先序遍历 头左右 -- 非递归
      */
-    public static void pre (BinaryNode<String> head) {
+    public static void pre (BinaryNode<Integer> head) {
         if (head == null) {
             return;
         }
-        Stack<BinaryNode<String>> stack = new Stack<>();
+        Stack<BinaryNode<Integer>> stack = new Stack<>();
         stack.add(head);
         while (!stack.isEmpty()) {
             head = stack.pop();
             // 弹出的时候打印
-            System.out.println(head.getValue() + " ");
+            System.out.print(head.getValue() + " -> ");
 
             // 压入的手先压入右孩子，再压入左孩子，这样弹出的时候就是先弹出左孩子
             if (head.getRight() != null) {
@@ -40,16 +40,16 @@ public class Code03_NonRecursiveTraversal {
     /**
      * 将先序遍历改写成  "头右左" 的实现 -- 非递归
      */
-    public static void pre01 (BinaryNode<String> head) {
+    public static void pre01 (BinaryNode<Integer> head) {
         if (head == null) {
             return;
         }
-        Stack<BinaryNode<String>> stack = new Stack<>();
+        Stack<BinaryNode<Integer>> stack = new Stack<>();
         stack.add(head);
         while (!stack.isEmpty()) {
             head = stack.pop();
             // 弹出的手打印
-            System.out.println(head.getValue() + " ");
+            System.out.print(head.getValue() + " -> ");
 
             // 压入的时候先压入左孩子，再压入右孩子， 这样弹出的时候就先弹出右孩子，这样就可以保证 "头右左" 的打印顺序
             if (head.getLeft() != null) {
@@ -67,12 +67,12 @@ public class Code03_NonRecursiveTraversal {
      * 最后依次弹出 stack02 中的节点，进行打印，就是左右头后续遍历方式了
      */
 
-    public static void pos01 (BinaryNode<String> head) {
+    public static void pos (BinaryNode<Integer> head) {
         if (head == null) {
             return;
         }
-        Stack<BinaryNode<String>> stack01 = new Stack<>();
-        Stack<BinaryNode<String>> stack02 = new Stack<>();
+        Stack<BinaryNode<Integer>> stack01 = new Stack<>();
+        Stack<BinaryNode<Integer>> stack02 = new Stack<>();
         stack01.push(head);
         while (!stack01.isEmpty()) {
             head = stack01.pop();
@@ -87,8 +87,16 @@ public class Code03_NonRecursiveTraversal {
         }
 
         while (!stack02.isEmpty()) {
-            System.out.println(stack02.pop().getValue() + " ");
+            System.out.print(stack02.pop().getValue() + " -> ");
         }
+    }
+
+    /**
+     * 后序遍历（如何只使用一个栈实现后序遍历） -- 非递归实现
+     * @TODO 很难
+     */
+    public static void pos02 (BinaryNode<String> head) {
+
     }
 
     /**
@@ -100,11 +108,11 @@ public class Code03_NonRecursiveTraversal {
      *
      *  ps: 中序遍历的思想就是，整个二叉树是可以被所有左边界给分解掉的，
      */
-    public static void in (BinaryNode<String> head) {
+    public static void in (BinaryNode<Integer> head) {
         if (head == null) {
             return;
         }
-        Stack<BinaryNode<String>> stack = new Stack<>();
+        Stack<BinaryNode<Integer>> stack = new Stack<>();
         // 这个 !stack.isEmpty() 用来处理第一个节点或者头结点进入循环用的，
         // 如果栈为空，head 也为空，那么循环就停了
         while (!stack.isEmpty() || head != null) {
@@ -115,18 +123,38 @@ public class Code03_NonRecursiveTraversal {
             } else {
                 // 左边界压完了，开始先弹出左边界，并打印
                 head = stack.pop();
-                System.out.println(head.getValue() + " ");
+                System.out.print(head.getValue() + " -> ");
                 // 弹出左边界后再去处理右子树
                 head = head.getRight();
             }
         }
     }
 
-    /**
-     * 后序遍历（如何只使用一个栈实现后序遍历） -- 非递归实现
-     * @TODO 很难
-     */
-    public static void pos02 (BinaryNode<String> head) {
+
+
+    public static void main(String[] args) {
+
+        BinaryNode<Integer> node = new BinaryNode<>(1);
+        node.setLeft(new BinaryNode<>(2));
+        node.getLeft().setLeft(new BinaryNode<>(4));
+        node.getLeft().setRight(new BinaryNode<>(5));
+
+        node.setRight(new BinaryNode<>(3));
+        node.getRight().setLeft(new BinaryNode<>(6));
+        node.getRight().getLeft().setLeft(new BinaryNode<>(7));
+        node.getRight().setRight(new BinaryNode<>(8));
+
+        // 先序遍历   1 -> 2 -> 4 -> 5 -> 3 -> 6 -> 7 -> 8
+        pre(node);
+        System.out.println("===============");
+        // 头右左遍历 1 -> 3 -> 8 -> 6 -> 7 -> 2 -> 5 -> 4
+        pre01(node);
+        System.out.println("===============");
+        // 中序遍历 4 -> 2 -> 5 -> 1 -> 7 -> 6 -> 3 -> 8
+        in(node);
+        System.out.println("===============");
+        // 后序遍历 4 -> 5 -> 2 -> 7 -> 6 -> 8 -> 3 -> 1
+        pos(node);
 
     }
 }
