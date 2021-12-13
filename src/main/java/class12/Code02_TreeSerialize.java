59db2c0f55194dcc5f8a1ee1b681fb180f1bbb91
zhangxin <jizhibao>
2021-12-13 16:25:43 +0800

    二叉树的先序序列化

---
+++ src/main/java/class12/Code02_TreeSerialize.java
@@ -0,0 +1,66 @@
+package class12;
+
+import class11.BinaryNode;
+import com.sun.deploy.util.StringUtils;
+
+import java.util.Stack;
+
+/**
+ * 二叉树的额序列化
+ *  只有先序和后序的序列化，，二叉树的中序序列化是不存在的，因为有歧义
+ * 序列化的过程实际就是将一棵二叉树转换成 字符串或者数组的过程，如果一个节点的孩子为空，也需要进行存储
+ * @Author 张三金
+ * @Date 2021/12/13 0013 10:44
+ * @Company jzb
+ * @Version 1.0.0
+ */
+public class Code02_TreeSerialize {
+
+    /**
+     *  先序遍历的方式进行序列化
+     *  使用逗号 "," 进行分隔
+     */
+    public static String preSerialize (BinaryNode<Integer> head) {
+        if (head == null) {
+            return null;
+        }
+        Stack<BinaryNode<Integer>> stack = new Stack<>();
+        stack.push(head);
+        String serializeString = "";
+        while (!stack.isEmpty()) {
+            head = stack.pop();
+
+            if (head == null) {
+                // 从栈中如果弹出来的数据为空的时候，同样需要设置一个 "null", 到字符串中去
+                serializeString += "null,";
+                continue;
+            } else {
+                serializeString +=  head.getValue() + ",";
+            }
+            // 这里如果左右孩子为空的话，同样的需要将空值压入栈中
+//            if (head.getRight() != null) {
+                stack.push(head.getRight());
+//            }
+//            if (head.getLeft() != null) {
+               stack.push(head.getLeft());
+//            }
+        }
+        return serializeString.substring(0, serializeString.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        BinaryNode<Integer> node1 = new BinaryNode<>(1);
+        node1.setLeft(new BinaryNode<>(2));
+        node1.getLeft().setLeft(new BinaryNode<>(4));
+        node1.getLeft().setRight(new BinaryNode<>(5));
+
+        node1.setRight(new BinaryNode<>(3));
+        node1.getRight().setLeft(new BinaryNode<>(6));
+        node1.getRight().getLeft().setLeft(new BinaryNode<>(7));
+        node1.getRight().setRight(new BinaryNode<>(8));
+
+        // 先序序列化  1,2,4,null,null,5,null,null,3,6,7,null,null,bull,8,null,null
+        System.out.println();
+        System.out.println(preSerialize(node1));
+    }
+}
---
+++ src/main/java/class12/Code03_TreeDeSerialize.java
@@ -0,0 +1,10 @@
+package class12;
+
+/**
+ * @Author 张三金
+ * @Date 2021/12/13 0013 10:45
+ * @Company jzb
+ * @Version 1.0.0
+ */
+public class Code03_TreeDeSerialize {
+}
