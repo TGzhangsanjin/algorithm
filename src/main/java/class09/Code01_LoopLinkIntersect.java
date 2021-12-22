0daa3b0b58e67330b623c82217599878397bb265
zhangxin <jizhibao>
2021-12-22 09:29:55 +0800

    判断一个链表是否有环

--- src/main/java/class09/Code01_LoopLinkIntersect.java
+++ src/main/java/class09/Code01_LoopLinkIntersect.java
@@ -12,12 +12,63 @@ import class04.SingleNode;
  *          a. 使用快慢指针 slow, fast, 从头结点开始遍历，慢指针每次走一个节点，快指针每次走两个节点
  *          b. 如果快指针最终能指向空，则说明链表无环，如果链表有环，那么慢指针和快指正一定会在环上的某一个节点相遇，这个时候停下来
  *          c. 第2个步骤中快指针和慢指针相遇后，将 fast 指向头结点，fast 和 slow 接着往下走，每次走一步，最终fast 和 slow 第一次相遇的节点就是入环节点
+ *      (2) 如果两个链表都是无环链表
+ *          a. 分别求出 head01 和 head02 的长度 length01 和 length02, 同时得出两个链表的结束节点 end01 和 end 02
+ *          b. 如果 end01 不等于 end02 那么说明两个链表肯定不相交，反之肯定相交
+ *          c. 根据第2步，如果判断出两个链表存在交点，则让长度更长的链表先走 |length01 - length02 |步， 知道相遇的第一个节点就是相交节点
+ *      (3) 如果一个建表有环，一个链表无环，那么二者绝不可能相交
+ *      (4) 如果两个链表同时有环
+ *          a. 找到 head01 和 head02 的入环节点，循环遍历其中一个head01，如果遍历完一圈后没有遇到 head02 的入环节点，则说明没有交点
+ *          b. 两个链表同时入环，而且可以得到这个入环节点 loopNode, 就可以把这个 loopNode 当成结束节点，使用（2）中的方法找到相交节点即可
+ *          c. 两个链表再不同的地方入环，相当于就是有两个相交节点，两个入环节点即可以看成第一次相交的节点，返回其中一个即可
  * @Author 张三金
  * @Date 2021/12/21 0021 11:37
  * @Company jzb
  * @Version 1.0.0
  */
 public class Code01_LoopLinkIntersect {
+
+
+    /**
+     * head01 和 head02 都是无环链表，返判断两个链表是否相交，如果相交则返回相交的第一个节点
+     */
+    public static SingleNode<Integer> noLoop (SingleNode<Integer> head01, SingleNode<Integer> head02) {
+        if (head01 == null || head02 == null) {
+            return null;
+        }
+        int length01 = 1;
+        int length02 = 1;
+        SingleNode<Integer> current01 = head01;
+        SingleNode<Integer> current02 = head02;
+        if (current01.getNext() != null) {
+            length01++;
+            current01 = current01.getNext();
+        }
+        if (current02.getNext() != null) {
+            length02++;
+            current02 = current02.getNext();
+        }
+        if (current01 != current02) {
+            // 结束节点不相同，则不会相交
+            return null;
+        }
+        // 把更长的链表赋值给 current01
+        current01 = length01 > length02 ? head01:head02;
+        // 把更短的链表赋值给 current02
+        current02 = length01 < length02 ? head01:head02;
+
+        // 让更长的链表先走 n 步
+        int n = Math.abs(length01 - length02);
+        while (n != 0) {
+            current01 = current01.getNext();
+            n--;
+        }
+        while (current01 != current02) {
+            current01 = current01.getNext();
+            current02 = current02.getNext();
+        }
+        return current01;
+    }
 
 
     /**
