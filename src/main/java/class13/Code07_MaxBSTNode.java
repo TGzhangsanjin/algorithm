bb101c426518bf217e632aa945fdf528e9309a79
Tgzhangsanjin <threegold.zhangsanjin@gmail.com>
2021-12-25 20:33:23 +0800

    返回最大搜索子树BST的头结点

--- src/main/java/class13/Code07_MaxBSTNode.java
+++ src/main/java/class13/Code07_MaxBSTNode.java
@@ -1,6 +1,9 @@
 package class13;
 
 import class11.BinaryNode;
+import class12.Code03_PrintBinaryTree;
+
+import java.util.ArrayList;
 
 /**
  * 给定一棵二叉树的头结点 head，返回这课二叉树中最大搜索子树的头结点
@@ -8,11 +11,11 @@ import class11.BinaryNode;
  * 和 Code06_MaxBSTCount 思路是一样的，只不过一个是返回最大搜索子树的节点个数，一个返回头结点
  *
  * 节点 x 的 Info 信息
- *      (1) isBCT 是否最大搜索子树
- *      (2) max 节点的最大值
- *      (3) min 节点的最小值
- *      (4) maxNode 最大搜索子树的头结点
- *      (4) maxNodeCount 最大搜索子树的节点个数
+ *      (1) maxBCTSize 最大搜索子树节点数
+ *      (2) size 最大节点数
+ *      (3) max 节点的最大值
+ *      (4) min 节点的最小值
+ *      (5) maxNode 最大搜索子树的头结点
  * @Author 张三金
  * @Date 2021/12/25 0025 17:29
  * @Company jzb
@@ -24,49 +27,140 @@ public class Code07_MaxBSTNode {
         if (head == null) {
             return null;
         }
+        return process(head).maxNode;
     }
 
     public static class Info {
-        boolean isBCT;
+        int maxBSTSize;
+        int size;
         int max;
         int min;
         BinaryNode<Integer> maxNode;
-        int maxNodeCount;
 
-        public Info (boolean isBCT, int max, int min, BinaryNode<Integer> maxNode, int maxNodeCount) {
-            this.isBCT = isBCT;
+        public Info (int maxBSTSize, int size, int max, int min, BinaryNode<Integer> maxNode) {
+            this.maxBSTSize = maxBSTSize;
+            this.size = size;
             this.max = max;
             this.min = min;
             this.maxNode = maxNode;
-            this.maxNodeCount = maxNodeCount;
         }
     }
 
     public static Info process (BinaryNode<Integer> node) {
-        // base case
         if (node == null) {
-            return new Info(true, 0, 0, null, 0);
+            return null;
         }
         Info leftInfo = process(node.getLeft());
         Info rightInfo = process(node.getRight());
-        boolean isBCT = false;
-        int max = Math.max(node.getValue(), Math.max(leftInfo.max, rightInfo.max));
-        int min = Math.min(node.getValue(), Math.max(leftInfo.min, rightInfo.min));
-        BinaryNode<Integer> maxNode;
-        int maxNodeCount;
-        if (leftInfo.isBCT && rightInfo.isBCT) {
-            if (node.getValue() > leftInfo.max && node.getValue() < leftInfo.min) {
-                // 只有这一种情况下当前节点才是搜索二叉树
-                isBCT = true;
-                maxNode = node;
-                maxNodeCount = leftInfo.maxNodeCount + rightInfo.maxNodeCount + 1;
-            } else {
-                if (leftInfo.maxNodeCount > rightInfo.maxNodeCount) {
-                    maxNode = leftInfo.maxNode;
-                    maxNodeCount = leftInfo.maxNodeCount;
-                }
-            }
-        } else if ()
+        int max = node.getValue();
+        int min = node.getValue();
+        int size = 1;
+        int maxBSTSize = Math.max(leftInfo == null ? 0:leftInfo.maxBSTSize, rightInfo == null ? 0:rightInfo.maxBSTSize);
+        if (leftInfo != null) {
+            max = Math.max(max, leftInfo.max);
+            min = Math.min(min, leftInfo.min);
+            size += leftInfo.size;
+        }
+        if (rightInfo != null) {
+            max = Math.max(max, rightInfo.max);
+            min = Math.min(min, rightInfo.min);
+            size += rightInfo.size;
+        }
 
+        BinaryNode<Integer> maxNode;
+        if (leftInfo == null && rightInfo == null) {
+            maxNode = node;
+        } else if (leftInfo != null && rightInfo != null) {
+            maxNode = leftInfo.maxBSTSize >= rightInfo.maxBSTSize ? leftInfo.maxNode : rightInfo.maxNode;
+        } else if (rightInfo != null) {
+            maxNode = rightInfo.maxNode;
+        } else {
+            maxNode = leftInfo.maxNode;
+        }
+
+
+        boolean leftBST = leftInfo == null ? true : leftInfo.maxBSTSize == leftInfo.size;
+        boolean rightBST = rightInfo == null ? true : rightInfo.maxBSTSize == rightInfo.size;
+
+        if (leftBST && rightBST) {
+            boolean leftMaxLessX = leftInfo == null ? true : leftInfo.max < node.getValue();
+            boolean rightMinMoreX = rightInfo == null ? true : rightInfo.min > node.getValue();
+            if (leftMaxLessX && rightMinMoreX) {
+                int leftSize = leftInfo == null ? 0:leftInfo.maxBSTSize;
+                int rightSize = rightInfo == null ? 0:rightInfo.maxBSTSize;
+                maxBSTSize = 1 + leftSize + rightSize;
+                maxNode = node;
+            }
+        }
+        return new Info(maxBSTSize, size, max, min, maxNode);
     }
+
+    public static void main(String[] args) {
+        int testTimes = 10000;
+        int maxLevel = 10;
+        int maxValue = 100;
+        for (int i = 0; i < testTimes; i++) {
+            BinaryNode<Integer> head = generateBT(maxLevel, maxValue);
+            BinaryNode<Integer> node01 = maxBSTNode(head);
+            BinaryNode<Integer> node02 = maxSubBSTHead1(head);
+            if (node01 != node02) {
+                System.out.println("Opps!!!!!");
+                Code03_PrintBinaryTree.printTree(head);
+                System.out.println();
+                break;
+            }
+        }
+    }
+
+
+    // For Test
+    public static BinaryNode<Integer> maxSubBSTHead1(BinaryNode<Integer> head) {
+        if (head == null) {
+            return null;
+        }
+        if (getBSTSize(head) != 0) {
+            return head;
+        }
+        BinaryNode<Integer> leftAns = maxSubBSTHead1(head.getLeft());
+        BinaryNode<Integer> rightAns = maxSubBSTHead1(head.getRight());
+        return getBSTSize(leftAns) >= getBSTSize(rightAns) ? leftAns : rightAns;
+    }
+
+    public static int getBSTSize(BinaryNode<Integer> head) {
+        if (head == null) {
+            return 0;
+        }
+        ArrayList<BinaryNode<Integer>> arr = new ArrayList<>();
+        in(head, arr);
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i).getValue() <= arr.get(i - 1).getValue()){
+                return 0;
+            }
+        }
+        return arr.size();
+    }
+
+    public static void in(BinaryNode<Integer> head, ArrayList<BinaryNode<Integer>> arr) {
+        if (head == null) {
+            return;
+        }
+        in(head.getLeft(), arr);
+        arr.add(head);
+        in(head.getRight(), arr);
+    }
+
+    public static BinaryNode<Integer> generateBT (int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+    public static BinaryNode<Integer> generate (int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        BinaryNode<Integer> node = new BinaryNode<>((int)(Math.random() * maxValue));
+        node.setLeft(generate(level + 1, maxLevel, maxValue));
+        node.setRight(generate(level + 1, maxLevel, maxValue));
+        return node;
+    }
+
+
 }
