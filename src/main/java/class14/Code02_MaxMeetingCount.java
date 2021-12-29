8e7b3f50b2c65d56fef95d6d200ec2714bff5f37
zhangxin <jizhibao>
2021-12-29 10:02:20 +0800

    最大会议数问题对数器

--- src/main/java/class14/Code02_MaxMeetingCount.java
+++ src/main/java/class14/Code02_MaxMeetingCount.java
@@ -47,31 +47,51 @@ public class Code02_MaxMeetingCount {
     }
 
     public static void main(String[] args) {
-        Project[] projects = {new Project(1, 99), new Project(99, 100), new Project(100, 200)};
-        System.out.println(maxMeetingCount(projects));
+        int testTimes = 1000;
+        int maxSize = 100;
+        int maxValue = 100;
+        for (int i = 0; i < testTimes; i++) {
+            Project[] projects = generateProject(maxSize, maxValue);
+            int count01 = maxMeetingCount(projects);
+            int count02 = masMeetingCountTest(projects);
+            if (count01 != count02) {
+                System.out.println("Opps!!!!!!!");
+            }
+        }
+//        Project[] projects = {new Project(1,2), new Project(1,3)};
+//        System.out.println(masMeetingCountTest(projects));
     }
 
 
+    // For Test 纯暴力的方式递归遍历每一种可能
     public static int masMeetingCountTest (Project[] projects) {
-
+        return process(projects, 0, 0);
     }
 
+    // 这个done一定要理解清楚，递归的时候这个done不是父级去收集子级的信息，而是父级将信息传递给子级，最后再一层一层的返回
     // projects: 剩余的项目
     // done: 已经完成的项目个数
     // timeLine: 当前来到的时间点
     // 返回当前能够安排的最多项目宣讲个数
     public static int process (Project[] projects, int done, int timeLine) {
-
-        int ans = 0;
+        if (projects == null || projects.length < 1) {
+            return done;
+        }
+        int ans = done;
         for (int i = 0; i < projects.length; i++) {
             if (projects[i].start >= timeLine) {
-                ans +=
+                int currentCount = process(copyButExcept(projects, i), done + 1, projects[i].end);
+                ans = Math.max(currentCount, ans);
             }
         }
+        return ans;
     }
 
     // 去掉 projects 中 i位置的数，然后返回一个新的数组
     public static Project[] copyButExcept (Project[] projects, int i) {
+        if (projects == null || projects.length < 1) {
+            return null;
+        }
         Project[] copy = new Project[projects.length - 1];
         int index = 0;
         for (int j = 0; j < projects.length; j++) {
@@ -80,5 +100,21 @@ public class Code02_MaxMeetingCount {
             }
         }
         return copy;
+    }
+
+    // 随机生成一个 project 数组
+    public static Project[] generateProject (int maxSize, int maxValue) {
+        int size = (int) (Math.random() * maxSize) + 1;
+        Project[] projects = new Project[size];
+
+        for (int i = 0; i < size; i++) {
+            int v1 = (int) (Math.random() * maxValue) + 1;
+            int v2 = (int) (Math.random() * maxValue) + 1;
+            if (v1 == v2) {
+                v1++;
+            }
+            projects[i] = new Project(Math.min(v1, v2), Math.max(v1, v2));
+        }
+        return projects;
     }
 }
