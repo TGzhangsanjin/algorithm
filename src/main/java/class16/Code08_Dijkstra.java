0d9a8526d6e5ca2ba7b871a7c0674809fd6c7e52
zhangxin <jizhibao>
2022-01-09 16:10:08 +0800

    dijkstra 迪杰斯特拉算法优化版本，

--- src/main/java/class16/Code08_Dijkstra.java
+++ src/main/java/class16/Code08_Dijkstra.java
@@ -63,4 +63,139 @@ public class Code08_Dijkstra {
         }
         return ans;
     }
+
+    /**
+     *  优化解法，使用一个加强堆处理，来维护接下来需要处理的节点
+     */
+    public Map<GraphNode<Integer>, Integer> dijkstra02 (GraphNode<Integer> from, int size) {
+        NodeHeap heap = new NodeHeap(size);
+        heap.addOrUpdateOrIgnore(from, 0);
+        Map<GraphNode<Integer>, Integer> resualt = new HashMap<>();
+        while (!heap.isEmpty()) {
+            NodeReacord reacord = heap.poll();
+            GraphNode<Integer> node = reacord.node;
+            for (GraphEdge<Integer> edge : node.getEdges()) {
+                heap.addOrUpdateOrIgnore(edge.getTo(), reacord.distance + edge.getWeight());
+            }
+            resualt.put(node, reacord.distance);
+        }
+        return resualt;
+    }
+
+    public class NodeHeap {
+
+        // 正常的堆的维护数组
+        private GraphNode<Integer>[] nodes;
+
+        // 反向索引表，用于记录数据在堆中的位置
+        // 注意，这里有一个小技巧点，如果数据从堆里弹出，不要remove，将value 设置为 -1， 这样 addOrUpdateOrIgnore 方法中的 ignore 就可以处理了
+        private Map<GraphNode<Integer>, Integer> heapIndexMap;
+
+        // 当前节点的距离
+        private Map<GraphNode<Integer>, Integer> distanceMap;
+
+        // 堆的大小
+        private int size;
+
+        public NodeHeap (int size) {
+            this.nodes = new GraphNode[size];
+            this.heapIndexMap = new HashMap<>(size);
+        }
+
+        public void addOrUpdateOrIgnore (GraphNode<Integer> node, int distance) {
+            if (inHeap(node)) {
+                // 在堆里面
+                // update
+                distanceMap.put(node, Math.min(distanceMap.get(node), distance));
+                int index = heapIndexMap.get(node);
+                heapInsert(index);
+            }
+            if (!isEnter(node)) {
+                // 没有进入过堆里面
+                // add
+                nodes[size] = node;
+                heapIndexMap.put(node, size);
+                distanceMap.put(node, distance);
+                heapInsert(size++);
+            }
+            // ignore
+        }
+
+        // 弹出
+        public NodeReacord poll () {
+            NodeReacord nodeRecord = new NodeReacord(nodes[0], distanceMap.get(nodes[0]));
+            nodes[0] = nodes[--size];
+            nodes[size] = null;
+            // 注意，反向索引表也需要更新
+            heapIndexMap.put(nodeRecord.node, -1);
+            heapIndexMap.put(nodes[0], 0);
+            heapify(0);
+            return nodeRecord;
+        }
+
+        public boolean isEmpty () {
+            return size == 0;
+        }
+
+        // 判断节点当前是否就在堆里面
+        private boolean inHeap (GraphNode<Integer> node) {
+            return heapIndexMap.get(node) != null && heapIndexMap.get(node) != -1;
+        }
+
+        // 判断节点是否进入过堆里
+        private boolean isEnter(GraphNode<Integer> node) {
+            return heapIndexMap.get(node) == null;
+        }
+
+        // 上浮index下标对应的数据
+        private void heapInsert (int index) {
+            while (distanceMap.get(nodes[((index - 1) / 2)]) > distanceMap.get(nodes[index])) {
+                swap(((index - 1) / 2), index);
+                index = ((index - 1) / 2);
+            }
+        }
+
+        // 下沉 index 下标对应的数据
+        private void heapify (int index) {
+            int left = index * 2 + 1;
+            while (left < size) {
+                int small = left + 1 < size && distanceMap.get(nodes[left + 1]) < distanceMap.get(nodes[left]) ? left + 1: left;
+                small = distanceMap.get(nodes[small]) < distanceMap.get(nodes[index]) ? small: index;
+                if (small == index) {
+                    break;
+                }
+                swap(small, index);
+                index = small;
+                left = index * 2 + 1;
+            }
+        }
+
+
+        private void swap (int i, int j) {
+            if (i == j) {
+                return;
+            }
+            GraphNode<Integer> nodeI = nodes[i];
+            GraphNode<Integer> nodeJ = nodes[j];
+            heapIndexMap.put(nodeI, j);
+            heapIndexMap.put(nodeJ, i);
+            nodes[i] = nodeJ;
+            nodes[j] = nodeI;
+        }
+
+    }
+
+
+    /**
+     * 放在加强堆中的元素
+     */
+    public class NodeReacord {
+        public GraphNode<Integer> node;
+        public int distance;
+
+        public NodeReacord (GraphNode<Integer> node, int distance) {
+            this.node = node;
+            this.distance = distance;
+        }
+    }
 }
