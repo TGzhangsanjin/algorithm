f2a326887d4077c7fd9ffadba058047035dcc82d
zhangxin <jizhibao>
2022-01-15 11:57:41 +0800

    数字字符串转换成字母字符串

---
+++ src/main/java/class18/Code05_ConvertToLetterString.java
@@ -0,0 +1,59 @@
+package class18;
+
+/**
+ * 字符串数字转换成字母
+ * 问题描述：
+ *  规定1和A对应、2和B对应、3和C对应 。。。 26和Z对应
+ *  那么一个数字字符串比如 "111", 可以转化为：“AAA”、“KA” 和 "AK"， 返回结果就是 3
+ *  "305" 就无法转化，因为 "0" 是无效的，“30” 也是无效的，所以返回结果就是 0
+ *  给定一个只有数字字符组组成的字符串 str, 返回有多少种转化结果
+ *
+ *
+ * @Author 张三金
+ * @Date 2022/1/15 0015 11:30
+ * @Company jzb
+ * @Version 1.0.0
+ */
+public class Code05_ConvertToLetterString {
+
+    public static int number (String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+
+        return process(str.toCharArray(), 0);
+
+    }
+
+    /**
+     * chars[0...i-1] 的转换可能性已经确定了，无需过问了
+     * chars[i....] 去转换，返回有多少种转换方法
+     */
+    public static int process (char[] chars, int index) {
+        if (index == chars.length) {
+            // 这里要返回1，说明啥，说明你顶到底了，都没有问题，那么这就是一条有效的路径，也就是一种可能性
+            return 1;
+        }
+        if (chars[index] <= '0' || chars[index] > '9') {
+            // 无效的字符串
+            return 0;
+        }
+        // 当前字符串独立处理
+        int p1 = process(chars, index + 1);
+
+        // 当前字符串和后面的字符串连在一起处理
+        int p2 = 0;
+        if (index + 1 != chars.length && ((chars[index] - '0') * 10 + (chars[index + 1] - '0')) < 27) {
+            // 1. index + 2 不能越界  2. index和index+1 两个连在一起组成的数字不能超过26
+            p2 = process(chars, index + 2);
+        }
+        return p1 + p2;
+    }
+
+    public static void main(String[] args) {
+        String str = "AAA";
+        System.out.println(number(str));
+    }
+
+
+}
