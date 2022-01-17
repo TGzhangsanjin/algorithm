e0969e8bfff77ddcec452c52f014921181cba288
zhangxin <jizhibao>
2022-01-17 11:42:25 +0800

    动态规划求最长回文序列的问题

--- src/main/java/class18/Code08_PalindromeSubsequence.java
+++ src/main/java/class18/Code08_PalindromeSubsequence.java
@@ -12,8 +12,7 @@ package class18;
 public class Code08_PalindromeSubsequence {
 
     /**
-     * 动态规划改写
-     * 其实就是就是把递归抽象成一个矩阵，现有递归的思路，才会有dp
+     * 求两个字符串的最长公共子序列
      */
     public static int lastDp (String text1, String text2) {
         char[] c1 = text1.toCharArray();
@@ -86,10 +85,10 @@ public class Code08_PalindromeSubsequence {
 
     /**
      * 返回 chars[left...right] 范围内的最长公共子序列，其实就是四种情况
-     * 1. 最长公共子序列chars[left] 和 chars[right]都不包含
-     * 2. 最长公共子序列包含 chars[left] 字符，不包含chars[right] 字符
-     * 3. 最长公共子序列不包含 chars[left] 字符，包含 chars[right] 字符
-     * 4. 最长公共子序列包含 chars[left] 和 chars[right] 字符, 那么 chars[left] == chars[right]
+     * 1. 最长回文子序列chars[left] 和 chars[right]都不包含
+     * 2. 最长回文子序列包含 chars[left] 字符，不包含chars[right] 字符
+     * 3. 最长回文子序列不包含 chars[left] 字符，包含 chars[right] 字符
+     * 4. 最长回文子序列包含 chars[left] 和 chars[right] 字符, 那么 chars[left] == chars[right]
      */
     public static int process2 (char[] chars, int left, int right) {
         if (left == right) {
@@ -113,9 +112,72 @@ public class Code08_PalindromeSubsequence {
         return Math.max(Math.max(p1, p2), Math.max(p3, p4));
     }
 
+    /**
+     * 动态规划的改写
+     */
+    public static int longestPalindromeSubseq03 (String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] chars = s.toCharArray();
+        int N = s.length();
+        int[][] dp = new int[N][N];
+        // 先把两条对角线求出来
+        dp[N - 1][N - 1] = 1;
+        for (int i = 0; i < N - 1; i++) {
+            dp[i][i] = 1;
+            dp[i][i+1] = chars[i] == chars[i+1] ? 2 : 1;
+        }
+        // 依赖关系
+        // left 依赖 left + 1,
+        // right 依赖 right - 1;
+        // 所以在表格中的遍历方向是： 从下往上，从左往右
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = i + 2; j < N; j++) {
+                // 分析可以得出， p1, p2, p3 分别对应表格中的左下角，左边，下边
+                // 换句话说每一个表格一定不小于他的左下角，左边，下边， 那么这个左下角其实就没比要去比较了，
+                // 因为 p1 一定不会超过 p2 和 p3
+//                int p1 = dp[i + 1][j-1];
+                int p2 = dp[i][j - 1];
+                int p3 = dp[i + 1][j];
+                int p4 = chars[i] == chars[j] ? 2 + dp[i+1][j-1]:0;
+                dp[i][j] = Math.max(p2, Math.max(p3, p4));
+            }
+        }
+        return dp[0][N - 1];
+    }
+
     public static void main(String[] args) {
-        String s = "cbbd";
-        System.out.println(longestPalindromeSubseq(s));
-        System.out.println(longestPalindromeSubseq02(s));
+        int testTImes = 1000;
+        int maxLength = 20;
+
+        System.out.println("test start");
+        for (int i = 0; i < testTImes; i++) {
+            String s1 = generateString(maxLength);
+            int count1 = longestPalindromeSubseq(s1);
+//            int count2 = longestPalindromeSubseq02(s1);
+            int count3= longestPalindromeSubseq03(s1);
+            if (count1 != count3) {
+                System.out.println("s1 = " + s1);
+                System.out.println("count1 = " + count1 + ", count3 = " + count3);
+                break;
+            }
+        }
+        System.out.println("test end");
+    }
+
+
+    /**
+     * 生成一个随机长度(0到maxLength - 1)的字符串
+     * @param maxLength
+     * @return
+     */
+    public static String generateString (int maxLength) {
+        int length = (int) (Math.random() * maxLength);
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            ans.append((char)((int) (Math.random() * 127)));
+        }
+        return ans.toString();
     }
 }
